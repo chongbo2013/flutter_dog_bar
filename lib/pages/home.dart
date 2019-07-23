a9bfc9e2a860eb1c9c72f56a55def1c5f9d07e2b
xufeifan <xufeifan@moxiu.net>
2019-07-23 18:24:16 +0800

    udpate
    Signed-off-by: xufeifan <xufeifan@moxiu.net>

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:flutter_dog_bar/utils/router.dart' as router;
+import 'about.dart';
 import 'dogbar.dart';
+import 'dogcollection.dart';
 
 //主页
 class Home extends StatefulWidget {
@@ -12,38 +14,60 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
-
+  int pageIndex=0;
   @override
   void initState() {
     super.initState();
 
   }
 
+  void refresh(int index){
+    pageIndex=index;
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('狗吧'),
       ),
-      body:DogBar(),
+      body:gotoPage(),
       drawer: Drawer(child: Column(
         children: <Widget>[
-          DrawerItem(1, '猜猜狗品种'),
-          DrawerItem(2, '养狗宝典'),
-          DrawerItem(3, '关于我们')
+          DrawerItem(1, '猜猜狗品种',refresh),
+          DrawerItem(2, '养狗宝典',refresh),
+          DrawerItem(3, '关于我们',refresh)
         ],
 
       ),)
     );
   }
-}
 
+  Widget gotoPage() {
+    Widget widget;
+    switch(pageIndex){
+      case 1:{
+        widget=DogBar();
+      };break;
+      case 2:{
+        widget=DogCollection();
+      };break;
+      case 3:{
+        widget=About();
+      };break;
+    }
+    return widget;
+  }
+}
+typedef DrawerCallback = void Function(int index);
 //抽屉
 class DrawerItem extends StatelessWidget {
   final int id;
   final String name;
+  //callback
+  @required DrawerCallback onDrawerPress;
 
-  DrawerItem(this.id, this.name);
+  DrawerItem(this.id, this.name,this.onDrawerPress);
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +79,9 @@ class DrawerItem extends StatelessWidget {
       ),
       height: 52.0,
       child: FlatButton(
-          onPressed: () {},
+          onPressed: () {
+            onDrawerPress(id);
+          },
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[Text(id.toString()), Text(' - '), Text(name)],
